8925d613e7ad77773fd5542852dbc4b3a9f6dc31
林威震 <weizhen19990727@gmail.com>
2019-12-02 21:58:00 +0800

    Revert "調整posts資料表結構的migration，down()要放與up()相反的指令"
    This reverts commit 88ed205750b998f6b609881956929e7a33b82e64.

--- database/migrations/2019_11_05_150339_add_is_feature_in_posts_table.php
+++ database/migrations/2019_11_05_150339_add_is_feature_in_posts_table.php
@@ -26,8 +26,6 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
-            $table ->string('title') ->change();
-            $table ->dropColumn('is_feature') ;
         });
     }
 }
